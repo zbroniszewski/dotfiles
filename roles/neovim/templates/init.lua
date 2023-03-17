de21fd02112aa9434aa3c1370a26e627b0ad0827
Zach Broniszewski <zachbroniszewski@gmail.com>
2023-03-17 19:50:43 -0400

    chore(neovim): remove yaml ansible schema in favor of `ansible-lint`

--- roles/neovim/templates/init.lua
+++ roles/neovim/templates/init.lua
@@ -70,7 +70,6 @@ return {
             schemas = {
               ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*.{yml,yaml}",
               ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
-              ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/**/tasks/*.{yml,yaml}",
               ["https://json.schemastore.org/circleciconfig.json"] = ".circleci/*.{yml,yaml}",
             },
           },
