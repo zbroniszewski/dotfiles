888aa1d3caa3227b399bd2ce487e3425ccfeb3e2
Zach Broniszewski <zachbroniszewski@gmail.com>
2023-03-17 19:49:16 -0400

    chore(neovim): format files

--- roles/neovim/templates/init.lua
+++ roles/neovim/templates/init.lua
@@ -1,17 +1,17 @@
 return {
   -- Configure AstroNvim updates
   updater = {
-    remote = "origin", -- remote to use
-    channel = "stable", -- "stable" or "nightly"
-    version = "latest", -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
-    branch = "main", -- branch name (NIGHTLY ONLY)
-    commit = nil, -- commit hash (NIGHTLY ONLY)
-    pin_plugins = nil, -- nil, true, false (nil will pin plugins on stable only)
-    skip_prompts = false, -- skip prompts about breaking changes
+    remote = "origin",     -- remote to use
+    channel = "stable",    -- "stable" or "nightly"
+    version = "latest",    -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
+    branch = "main",       -- branch name (NIGHTLY ONLY)
+    commit = nil,          -- commit hash (NIGHTLY ONLY)
+    pin_plugins = nil,     -- nil, true, false (nil will pin plugins on stable only)
+    skip_prompts = false,  -- skip prompts about breaking changes
     show_changelog = true, -- show the changelog after performing an update
     -- auto_reload = false, -- automatically reload and sync packer after a successful update
-    auto_quit = false, -- automatically quit the current session after a successful update
-    remotes = { -- easily add new remotes to track
+    auto_quit = false,     -- automatically quit the current session after a successful update
+    remotes = {            -- easily add new remotes to track
       --   ["remote_name"] = "https://remote_url.come/repo.git", -- full remote url
       --   ["remote2"] = "github_user/repo", -- GitHub user/repo shortcut,
       --   ["remote3"] = "github_user", -- GitHub user assume AstroNvim fork
@@ -29,7 +29,7 @@ return {
     formatting = {
       -- control auto formatting on save
       format_on_save = {
-        enabled = true, -- enable or disable format on save globally
+        enabled = true,     -- enable or disable format on save globally
         allow_filetypes = { -- enable format on save for specified filetypes only
           -- "go",
         },
@@ -88,7 +88,14 @@ return {
     performance = {
       rtp = {
         -- customize default disabled vim plugins
-        disabled_plugins = { "tohtml", "gzip", "matchit", "zipPlugin", "netrwPlugin", "tarPlugin", "matchparen" },
+        disabled_plugins = {
+          "tohtml",
+          "gzip",
+          "matchit",
+          "zipPlugin",
+          "netrwPlugin",
+          "tarPlugin",
+        },
       },
     },
   },
@@ -111,24 +118,30 @@ return {
     local function yaml_ft(path, bufnr)
       -- get content of buffer as string
       local content = vim.filetype.getlines(bufnr)
-      if type(content) == "table" then content = table.concat(content, "\n") end
+      if type(content) == "table" then
+        content = table.concat(content, "\n")
+      end
 
       -- check if file is in roles, tasks, or handlers folder
-      local path_regex = vim.regex "(tasks\\|roles\\|handlers)/"
-      if path_regex and path_regex:match_str(path) then return "yaml.ansible" end
+      local path_regex = vim.regex("(tasks\\|roles\\|handlers)/")
+      if path_regex and path_regex:match_str(path) then
+        return "yaml.ansible"
+      end
       -- check for known ansible playbook text and if found, return yaml.ansible
-      local regex = vim.regex "hosts:\\|tasks:"
-      if regex and regex:match_str(content) then return "yaml.ansible" end
+      local regex = vim.regex("hosts:\\|tasks:")
+      if regex and regex:match_str(content) then
+        return "yaml.ansible"
+      end
 
       -- return yaml if nothing else
       return "yaml"
     end
 
-    vim.filetype.add {
+    vim.filetype.add({
       extension = {
         yml = yaml_ft,
         yaml = yaml_ft,
       },
-    }
+    })
   end,
 }
--- roles/neovim/templates/plugins/null-ls.lua
+++ roles/neovim/templates/plugins/null-ls.lua
@@ -2,21 +2,21 @@ return {
   "jose-elias-alvarez/null-ls.nvim",
   opts = function(_, config)
     -- config variable is the default configuration table for the setup function call
-    local null_ls = require "null-ls"
+    local null_ls = require("null-ls")
 
     -- Check supported formatters and linters
     -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
     -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
     config.sources = {
       -- Set a formatter
-      null_ls.builtins.formatting.stylua.with {
+      null_ls.builtins.formatting.stylua.with({
         extra_args = {
           "--indent-type",
           "Spaces",
           "--indent-width",
           "2",
         },
-      },
+      }),
       null_ls.builtins.formatting.prettier,
       null_ls.builtins.formatting.eslint_d,
       null_ls.builtins.formatting.phpcsfixer,
@@ -33,7 +33,7 @@ return {
       null_ls.builtins.diagnostics.eslint_d,
       null_ls.builtins.diagnostics.php,
       null_ls.builtins.diagnostics.shellcheck,
-      null_ls.builtins.diagnostics.yamllint.with {
+      null_ls.builtins.diagnostics.yamllint.with({
         args = {
           "--format",
           "parsable",
@@ -41,7 +41,7 @@ return {
           "{extends: default, rules: {line-length: {max: 140}}}",
           "-",
         },
-      },
+      }),
       -- null_ls.builtins.diagnostics.perlimports,
       null_ls.builtins.diagnostics.hadolint,
       null_ls.builtins.diagnostics.golangci_lint,
