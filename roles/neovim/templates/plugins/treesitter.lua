ac640cdb8acedfa274cc1567d4512223566e4117
Zach Broniszewski <zachbroniszewski@gmail.com>
2023-03-23 21:06:49 -0400

    feat(neovim): disable nvim-rainbow

--- roles/neovim/templates/plugins/treesitter.lua
+++ roles/neovim/templates/plugins/treesitter.lua
@@ -1,5 +1,17 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  -- dependencies = {
+  --   "HiPhish/nvim-ts-rainbow2",
+  --   config = function()
+  --     -- HACK: https://github.com/p00f/nvim-ts-rainbow/issues/112#issuecomment-1310835936
+  --     vim.api.nvim_create_autocmd({ "BufWritePost", "FocusGained" }, {
+  --       callback = function()
+  --         vim.cmd("TSDisable rainbow")
+  --         vim.cmd("TSEnable rainbow")
+  --       end,
+  --     })
+  --   end,
+  -- },
   opts = {
     ensure_installed = {
       "lua",
@@ -15,10 +27,10 @@ return {
       "go",
       "python",
     },
-    rainbow = {
-      enable = true,
-      extended_mode = true,
-      max_file_lines = nil,
-    },
+    -- rainbow = {
+    --   enable = true,
+    --   extended_mode = true,
+    --   max_file_lines = nil,
+    -- },
   },
 }
