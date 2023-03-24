fc72c75e57ffb6e0ee08306078807ce11b081321
Zach Broniszewski <zachbroniszewski@gmail.com>
2023-03-23 21:01:15 -0400

    feat(neovim): import astrocommunity packages

--- roles/neovim/templates/plugins/community.lua
+++ roles/neovim/templates/plugins/community.lua
@@ -6,4 +6,9 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.diagnostics.lsp_lines-nvim" },
+  { import = "astrocommunity.diagnostics.trouble-nvim" },
 }
