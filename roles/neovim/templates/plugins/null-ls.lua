f03ecfa329317738cf58792335a05248e3cf0dc7
Zach Broniszewski <zachbroniszewski@gmail.com>
2023-04-20 08:25:36 -0400

    fix(neovim): fix duplicate lsp attach issues

--- roles/neovim/templates/plugins/null-ls.lua
+++ roles/neovim/templates/plugins/null-ls.lua
@@ -1,6 +1,6 @@
 return {
   "jose-elias-alvarez/null-ls.nvim",
-  opts = function(_, config)
+  opts = function(_, _)
     -- config variable is the default configuration table for the setup function call
     local null_ls = require("null-ls")
 
@@ -8,27 +8,23 @@ return {
     -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
     -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 
-    config.sources = {
-      null_ls.builtins.formatting.stylua.with({
-        extra_args = {
-          "--indent-type",
-          "Spaces",
-          "--indent-width",
-          "2",
-        },
-      }),
-
-      null_ls.builtins.diagnostics.yamllint.with({
-        args = {
-          "--format",
-          "parsable",
-          "--config-data",
-          "{extends: default, rules: {line-length: {max: 140}}}",
-          "-",
-        },
-      }),
-    }
-
-    return config -- return final config table
+    null_ls.register(null_ls.builtins.formatting.stylua.with({
+      extra_args = {
+        "--indent-type",
+        "Spaces",
+        "--indent-width",
+        "2",
+      },
+    }))
+    null_ls.deregister(null_ls.builtins.diagnostics.yamllint)
+    null_ls.register(null_ls.builtins.diagnostics.yamllint.with({
+      args = {
+        "--format",
+        "parsable",
+        "--config-data",
+        "{extends: default, rules: {line-length: {max: 120}}}",
+        "-",
+      },
+    }))
   end,
 }
