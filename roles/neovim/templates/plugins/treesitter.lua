cb782a647ae3415a9a807e7ae73580909d6391b0
Zach Broniszewski <zachbroniszewski@gmail.com>
2023-08-16 22:41:38 -0400

    chore(neovim): remove `nvim-ts-rainbow2` dependency

--- roles/neovim/templates/plugins/treesitter.lua
+++ roles/neovim/templates/plugins/treesitter.lua
@@ -3,18 +3,6 @@ local utils = require("astronvim.utils")
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    -- dependencies = {
-    --   "HiPhish/nvim-ts-rainbow2",
-    --   config = function()
-    --     -- HACK: https://github.com/p00f/nvim-ts-rainbow/issues/112#issuecomment-1310835936
-    --     vim.api.nvim_create_autocmd({ "BufWritePost", "FocusGained" }, {
-    --       callback = function()
-    --         vim.cmd("TSDisable rainbow")
-    --         vim.cmd("TSEnable rainbow")
-    --       end,
-    --     })
-    --   end,
-    -- },
     opts = function(_, opts)
       -- local ft_to_parser = require("nvim-treesitter.parsers").filetype_to_parsername
       -- ft_to_parser["terraform"] = "terraform"
@@ -45,10 +33,5 @@ return {
         "toml",
       })
     end,
-    -- rainbow = {
-    --   enable = true,
-    --   extended_mode = true,
-    --   max_file_lines = nil,
-    -- },
   },
 }
