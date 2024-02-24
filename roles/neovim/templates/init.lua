bf33823dd270059031e79e4f02ec4a98d876234c
Zach Broniszewski <zachbroniszewski@gmail.com>
2024-02-24 11:55:21 -0500

    feat(neovim): increase code format timeout

--- roles/neovim/templates/init.lua
+++ roles/neovim/templates/init.lua
@@ -43,7 +43,7 @@ return {
       disabled = { -- disable formatting capabilities for the listed language servers
         -- "sumneko_lua",
       },
-      timeout_ms = 1000, -- default format timeout
+      timeout_ms = 5000, -- default format timeout
       -- filter = function(client) -- fully override the default formatting function
       --   return true
       -- end
